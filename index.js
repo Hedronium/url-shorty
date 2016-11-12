e5f5b81763a622b3c7b158470e8d45495741cb91
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-11-12 19:16:58 +0600

    new update on url

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ var Core = require('./lib/core');
 
 core = new Core();
 
-console.log(core.encodeintstring62('https://google.com/',12545353452264563457287464));
-console.log(core.decodeintstring62('https://google.com/','cb'));
-
-console.log(core.encodeint62(12545353452264563457287464));
-console.log(core.decodeint62('cb'));
+module.exports = core;
